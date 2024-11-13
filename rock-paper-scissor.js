3b15c483d78c7806fb721865a87c27754294ff8f
Yimin Guo <81876378+GuoYimin97@users.noreply.github.com>
2024-11-13 12:17:32 -0600

    Add some style and revise the functionality of RSP, still WIP

--- rock-paper-scissor.js
+++ rock-paper-scissor.js
@@ -1,7 +1,7 @@
 // console.log("test test babe")
 // test test the branch thing
 
-// step 2: Get computer choice
+// step 1: Get computer choice
 function getComputerChoice() {
     const randomNumber = Math.random();
     console.log("The random number generated is " + randomNumber);
@@ -15,30 +15,26 @@ function getComputerChoice() {
     }
 
     console.log("Computer's choice is " + computerChoice)
+
+    nround++;
+    const containerComputerChoice = document.querySelector(".computer-choice");
+    if (nround > 1) {
+        containerComputerChoice.lastChild.remove();
+    }
+
+    const elComputerChoice = document.createElement("div");
+    elComputerChoice.textContent = computerChoice;
+    containerComputerChoice.appendChild(elComputerChoice);
+
     return computerChoice;
 }
 
-// step 3: Get human choice
-const properHumanInput = ["rock", "paper", "scissor"];
-
-function getHumanChoice() {
-    let humanChoice = window.prompt("Alright what's your move?").toLowerCase();
-    
-    if (!properHumanInput.includes(humanChoice)) {
-        humanChoice = "invalid!";
-    }
-
-    console.log("Human choice is " + humanChoice);
-    return humanChoice;
-}
-
-// console.log("Human choice is " + getHumanChoice());
-
-// Step 4: Declare the players score variables in global scope
+// Step 2: Declare the players score variables in global scope
+let nround = 0;
 let computerScore = 0;
 let humanScore = 0;
 
-// Step 5: Write the logic to play a single round
+// Step 3: Write the logic to play a single round
 function playRound(computerChoice, humanChoice) {
     if (humanChoice === "invalid!") {
         winner = null;
@@ -54,17 +50,62 @@ function playRound(computerChoice, humanChoice) {
         } else if (computerChoice === "scissor") {
             winner = (humanChoice === "rock") ? "human" : "computer";
         }
-    } 
-    
+    }
+
     if (winner === "computer") {
         computerScore++;
     } else if (winner === "human") {
         humanScore++;
     }
+    
+    const containerHumanChoice = document.querySelector(".human-choice");
+    if (nround > 1) {
+        containerHumanChoice.lastChild.remove();
+    }
+    
+    const elHumanChoice = document.createElement("div");
+    elHumanChoice.textContent = "Your choice of the round is " + humanChoice;
+    containerHumanChoice.appendChild(elHumanChoice);
+    
+    const containerBody = document.querySelector("body");
+    if (nround > 1) {
+        containerBody.lastChild.remove();
+    }
 
-    // console.log("The winner of the round is " + winner);
+    const elRoundResult = document.createElement("div");
+    elRoundResult.textContent = "The winner of round " + nround + " is " + winner;
+    containerBody.appendChild(elRoundResult);
+
+    console.log("It's round " + nround + ", and the round winner is: " + winner);
+    
+    console.log("It's round " + nround + ", and computer's current score is " + computerScore);
+    console.log("It's round " + nround + ", and human's current score is " + humanScore);
+
+    if (computerScore === 5) {
+        finalWinner = "computer";
+        console.log("Computer has won the game.");
+    } else if (humanScore === 5) {
+        finalWinner = "human";
+        console.log("YOU WON THE GAME!!!")
+    }
+
     return winner;
 }
+
+// step 4: Get human choice from button and run the round
+const btnRock = document.querySelector("#btnrock");
+btnRock.addEventListener("click", () => {
+    playRound(getComputerChoice(), "rock");
+});
+const btnPaper = document.querySelector("#btnpaper");
+btnPaper.addEventListener("click", () => {
+    playRound(getComputerChoice(), "paper");
+});
+const btnScissor = document.querySelector("#btnscissor");
+btnScissor.addEventListener("click", () => {
+    playRound(getComputerChoice(), "scissor");
+});
+
 
 // Step 6: Write the logic to play the entire game
 function playGame() {
@@ -78,8 +119,8 @@ function playGame() {
         }
         console.log("It's round " + i + ", and computer's current score is " + computerScore);
         console.log("It's round " + i + ", and human's current score is " + humanScore);
-      }
-    
+    }
+
     if (computerScore > humanScore) {
         finalWinner = "computer";
     } else if (computerScore < humanScore) {
@@ -91,7 +132,7 @@ function playGame() {
     return finalWinner;
 }
 
-console.log("The final winner of the game is " + playGame());
+// console.log("The final winner of the game is " + playGame());
 
 // Test test
 // myComputerChoice = getComputerChoice();
